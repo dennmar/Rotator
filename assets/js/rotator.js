5157dcbc4cad7f0cbb44f7648fb5067e6ca00408
dennmar <dennismariano3@gmail.com>
2017-07-03 18:07:17 -0700

    Added documentation for JavaScript functions and made each line 80 characters maximum

--- assets/js/rotator.js
+++ assets/js/rotator.js
@@ -1,16 +1,33 @@
 var boardSize = 5;
 
-$( ".square" ).on( "click", function() {
-	$( this ).toggleClass( "rotated" );
-	if ( checkWin() ) {
-		console.log( "YOU WON!!!!!!!!" );
-	}
-});
+init();
 
+/**
+ * Initializes the board and event listeners for the squares.
+ */
+function init() {
+	$( ".square" ).on( "click", function() {
+		$( this ).toggleClass( "rotated" );
+		if ( checkWin() ) {
+			console.log( "YOU WON!!!!!!!!" );
+		}
+	});
+}
+
+/**
+ * Checks if the player has won the game by changing all squares to the same 
+ *   color.
+ * @return True if the player has won, false otherwise.
+ */
 function checkWin() {  
 	return allUnrotated() || allRotated();
 }
 
+/**
+ * Checks if all squares are the color of an unrotated square.
+ * @return True if all squares are the color of an unrotated square, false 
+ *         otherwise.
+ */
 function allUnrotated() {
 	// checks if all squares are the color of an unrotated square
 	for ( var r = 0; r < boardSize; r++ ) {
@@ -25,8 +42,12 @@ function allUnrotated() {
   return true;
 }
 
+/**
+ * Checks if all squares are the color of a rotated square.
+ * @return True if all squares are the color of a rotated square, false 
+ *         otherwise.
+ */
 function allRotated() {
-	// checks if all squares are the color of a rotated square
 	for ( var r = 0; r < boardSize; r++ ) {
   	for ( var c = 0; c < boardSize; c++ ) {
   		var squareId = "#r" + r + "c" + c;
