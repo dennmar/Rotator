774e9169b268d4e2f283ae67782d68d668928f97
dennmar <dennismariano3@gmail.com>
2017-08-02 16:39:23 -0700

    Create main.css and apply styles to start menu

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var port        = process.env.PORT || 8000;
 
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.set( "view engine", "ejs" );
+app.use( express.static( __dirname + "/public" ) );
 
 app.get( "/", function( req, res ) {
 	res.render( "start" );
