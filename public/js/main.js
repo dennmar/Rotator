60426cd29cfa339c7b448156dbbafb2d9c1e5db2
dennmar <dennismariano3@gmail.com>
2017-08-02 18:20:18 -0700

    Update icon hover to light up the respective label

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-console.log( "Hello world!" );
+/**
+ * Brings the labels for all icons to full opacity when their respective
+ *     icon is hovered over.
+ */
+function setIconHover() {
+	$( ".labeled-icon i" ).on( "mouseenter", function() {
+		var labelSel = "label[for=" + $( this ).attr( "id" ) + "]";
+		$( labelSel ).addClass( "full-opacity" );
+	});
+
+	$( ".labeled-icon i" ).on( "mouseleave", function() {
+		var labelSel = "label[for=" + $( this ).attr( "id" ) + "]";
+		$( labelSel ).removeClass( "full-opacity" );
+	});
+}
+
+setIconHover();
