7ccfca05aab087e573dbc87220819283f86cd889
dennmar <dennismariano3@gmail.com>
2017-08-10 19:00:35 -0700

    Update log in/sign up page to highlight the title of the selected form green

--- public/js/user.js
+++ public/js/user.js
@@ -1,16 +1,24 @@
 $( "input[name='option']" ).on( "change", function() {
 	if ( $( this ).val() === "log-in" ) {
+		$( "label[for='sign-up-radio']" ).removeClass( "green" );
+		$( "label[for='log-in-radio']" ).addClass( "green" );
+
 		$( "span.log-in" ).fadeOut( 0, function() {
 			$( "form.log-in" ).fadeIn( 1000 );
 		});
+
 		$( "form.sign-up" ).fadeOut( 0, function() {
 			$( "span.sign-up" ).fadeIn( 1000 );
 		});
 	}
 	else {
+		$( "label[for='log-in-radio']" ).removeClass( "green" );
+		$( "label[for='sign-up-radio']" ).addClass( "green" );
+
 		$( "span.sign-up" ).fadeOut( 0, function() {
 			$( "form.sign-up" ).fadeIn( 1000 );
 		});
+
 		$( "form.log-in" ).fadeOut( 0, function() {
 			$( "span.log-in" ).fadeIn( 1000 );
 		});
