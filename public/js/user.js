42b19f40a7037cd2293588a7443525137ff5e905
dennmar <dennismariano3@gmail.com>
2017-08-10 18:33:45 -0700

    Update log in/sign up page to use fading effects when displaying or hiding the forms

--- public/js/user.js
+++ public/js/user.js
@@ -1,12 +1,18 @@
 $( "input[name='option']" ).on( "change", function() {
 	if ( $( this ).val() === "log-in" ) {
-		$( ".log-in" ).toggleClass( "removed" );
-		$( "span.sign-up" ).removeClass( "removed" );
-		$( "form.sign-up" ).addClass( "removed" );
+		$( "span.log-in" ).fadeOut( 0, function() {
+			$( "form.log-in" ).fadeIn( 1000 );
+		});
+		$( "form.sign-up" ).fadeOut( 0, function() {
+			$( "span.sign-up" ).fadeIn( 1000 );
+		});
 	}
 	else {
-		$( ".sign-up" ).toggleClass( "removed" );
-		$( "span.log-in" ).removeClass( "removed" );
-		$( "form.log-in" ).addClass( "removed" );
+		$( "span.sign-up" ).fadeOut( 0, function() {
+			$( "form.sign-up" ).fadeIn( 1000 );
+		});
+		$( "form.log-in" ).fadeOut( 0, function() {
+			$( "span.log-in" ).fadeIn( 1000 );
+		});
 	}
 });
