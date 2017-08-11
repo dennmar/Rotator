491ad5b9966a62c5b24b34be769bd8a1157388b5
dennmar <dennismariano3@gmail.com>
2017-08-11 16:11:52 -0700

    Update log in/sign up page to display sign up error flash message if an error occurs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express                = require( "express" );
 var mongoose               = require( "mongoose" );
 var passport               = require( "passport" );
 var bodyParser             = require( "body-parser" );
+var flash                  = require( "connect-flash" );
 var User                   = require( "./models/user" );
 var LocalStrategy          = require( "passport-local" );
 var passportLocalMongoose  = require( "passport-local-mongoose" );
@@ -17,6 +18,7 @@ mongoose.Promise = global.Promise;
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.set( "view engine", "ejs" );
 app.use( express.static( __dirname + "/public" ) );
+app.use( flash() );
 app.use( require( "express-session" )( {
 	secret: "cozy lummox gives smart squid who asked for job pen",
 	resave: false,
@@ -28,6 +30,12 @@ app.use( passport.session() );
 passport.use( new LocalStrategy( User.authenticate() ) );
 passport.serializeUser( User.serializeUser() );
 passport.deserializeUser( User.deserializeUser() );
+
+app.use( function( req, res, next ) {
+	res.locals.logInError = req.flash( "logInError" );
+	res.locals.signInError = req.flash( "signInError" );
+	next();
+});
 
 app.get( "/", function( req, res ) {
 	res.render( "start" );
@@ -54,7 +62,8 @@ app.post( "/user/signup", function( req, res ) {
 	var newUser = new User( { username: req.body.username } );
 	User.register( newUser, req.body.password, function( err, user ) {
 		if ( err ) {
-			console.log( err );
+			var errorMessage = err.message + ".";
+			req.flash( "signInError", errorMessage );
 			res.redirect( "/user" );
 		}
 		else {
