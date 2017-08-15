5bd0f736c4d205bf8fdd0d1154ebec89f71b20b3
dennmar <dennismariano3@gmail.com>
2017-08-14 23:12:33 -0700

    Update user.js to have form display logic enclosed in a function

--- public/js/user.js
+++ public/js/user.js
@@ -1,26 +1,33 @@
-$( "input[name='option']" ).on( "change", function() {
-	if ( $( this ).val() === "log-in" ) {
-		$( "label[for='sign-up-radio']" ).removeClass( "green" );
-		$( "label[for='log-in-radio']" ).addClass( "green" );
+/**
+ * Shows the selected form for the user and hides the other form.
+ */
+function setFormDisplay() {
+	$( "input[name='option']" ).on( "change", function() {
+		if ( $( this ).val() === "log-in" ) {
+			$( "label[for='sign-up-radio']" ).removeClass( "green" );
+			$( "label[for='log-in-radio']" ).addClass( "green" );
 
-		$( "span.log-in" ).fadeOut( 0, function() {
-			$( "form.log-in" ).fadeIn( 250 );
-		});
+			$( "span.log-in" ).fadeOut( 0, function() {
+				$( "form.log-in" ).fadeIn( 250 );
+			});
 
-		$( "form.sign-up" ).fadeOut( 0, function() {
-			$( "span.sign-up.description" ).fadeIn( 250 );
-		});
-	}
-	else {
-		$( "label[for='log-in-radio']" ).removeClass( "green" );
-		$( "label[for='sign-up-radio']" ).addClass( "green" );
+			$( "form.sign-up" ).fadeOut( 0, function() {
+				$( "span.sign-up.description" ).fadeIn( 250 );
+			});
+		}
+		else {
+			$( "label[for='log-in-radio']" ).removeClass( "green" );
+			$( "label[for='sign-up-radio']" ).addClass( "green" );
 
-		$( "span.sign-up" ).fadeOut( 0, function() {
-			$( "form.sign-up" ).fadeIn( 250 );
-		});
+			$( "span.sign-up" ).fadeOut( 0, function() {
+				$( "form.sign-up" ).fadeIn( 250 );
+			});
 
-		$( "form.log-in" ).fadeOut( 0, function() {
-			$( "span.log-in.description" ).fadeIn( 250 );
-		});
-	}
-});
+			$( "form.log-in" ).fadeOut( 0, function() {
+				$( "span.log-in.description" ).fadeIn( 250 );
+			});
+		}
+	});
+}
+
+setFormDisplay();
