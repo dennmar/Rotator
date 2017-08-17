2bb97ae7ed2eac7ec7aaf71f7a392470c06e82f5
dennmar <dennismariano3@gmail.com>
2017-08-17 15:26:16 -0700

    Create index.js for menu and game routes

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express  = require( "express" );
+var router   = express.Router();
+var modes    = [ "easy", "medium", "hard" ];
+
+router.get( "/", function( req, res ) {
+	res.render( "start" );
+});
+
+router.get( "/game", function( req, res ) {
+	res.render( "game" );
+});
+
+router.get( "/game/:mode", function( req, res ) {
+	var mode = req.params.mode;
+	modes.forEach( function( difficulty ) {
+		if ( mode === difficulty ) {
+			res.render( req.params.mode );
+		}
+	});
+});
+
+router.get( "/levels", function( req, res ) {
+	res.render( "levels" );
+});
+
+router.get( "/levels/:level", function( req, res) {
+	res.render( "level", { level: req.params.level } );
+});
+
+module.exports = router;
