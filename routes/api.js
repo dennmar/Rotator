b530ccb40aadd051c8412b3df61fc986595ea0ef
dennmar <dennismariano3@gmail.com>
2017-08-18 20:50:18 -0700

    Remove api routes that do not contribute to functionality of Rotator

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,6 @@ var router   = express.Router();
 var User     = require( "../models/user" );
 var Level    = require( "../models/level" );
 
-router.get( "/users", function( req, res ) {
-	User.find( function( err, users ) {
-		if ( err ) {
-			res.send( err );
-		}
-		res.json( users );
-	});
-});
-
 router.get( "/user", function( req, res ) {
 	if ( !req.user ) {
 		res.json( {} );
@@ -46,49 +37,6 @@ router.get( "/levels", function( req, res ) {
 			res.send( err );
 		}
 		res.json( levels );
-	});
-});
-
-router.post( "/levels", function( req, res ) {
-	var newLevel = {
-		level: req.body.level,
-		difficulty: req.body.difficulty,
-		startingRotates: req.body.startingRotates
-	};
-	Level.create( newLevel, function( err, level ) {
-		if ( err ) {
-			console.log( err );
-		}
-		else {
-			res.redirect( "/api/levels" );
-		}
-	});
-});
-
-router.put( "/levels/:level_id", function( req, res ) {
-	var updatedLevel = {
-		level: req.body.level,
-		difficulty: req.body.difficulty,
-		startingRotates: req.body.startingRotates
-	}
-	Level.findByIdAndUpdate( req.params.level_id, updatedLevel, function( err, updatedLevel ) {
-		if ( err ) {
-			console.log( err );
-		}
-		else {
-			res.redirect( "/api/levels" );
-		}
-	});
-});
-
-router.delete( "/levels/:level_id", function( req, res ) {
-	Level.findByIdAndRemove( req.params.level_id, function( err ) {
-		if ( err ) {
-			console.log( err );
-		}
-		else {
-			res.redirect( "/api/levels" );
-		}
 	});
 });
 
