f9d9e87261c3503d718c9e3c65f21e7d3d2b41cf
dennmar <dennismariano3@gmail.com>
2017-08-29 16:30:27 -0700

    Update to include post, put, and delete routes for levels temporarily

--- routes/find.js
+++ routes/find.js
@@ -40,4 +40,47 @@ router.get( "/levels", function( req, res ) {
 	});
 });
 
+router.post( "/levels", function( req, res ) {
+	var newLevel = {
+		level: req.body.level,
+		difficulty: req.body.difficulty,
+		startingRotates: req.body.startingRotates
+	};
+	Level.create( newLevel, function( err, level ) {
+		if ( err ) {
+			console.log( err );
+		}
+		else {
+			res.redirect( "/find/levels" );
+		}
+	});
+});
+
+router.put( "/levels/:level_id", function( req, res ) {
+	var updatedLevel = {
+		level: req.body.level,
+		difficulty: req.body.difficulty,
+		startingRotates: req.body.startingRotates
+	}
+	Level.findByIdAndUpdate( req.params.level_id, updatedLevel, function( err, updatedLevel ) {
+		if ( err ) {
+			console.log( err );
+		}
+		else {
+			res.redirect( "/find/levels" );
+		}
+	});
+});
+
+router.delete( "/levels/:level_id", function( req, res ) {
+	Level.findByIdAndRemove( req.params.level_id, function( err ) {
+		if ( err ) {
+			console.log( err );
+		}
+		else {
+			res.redirect( "/find/levels" );
+		}
+	});
+});
+
 module.exports = router;
