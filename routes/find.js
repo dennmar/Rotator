99084402ac80abae51d7a0a412ea9d2a1305a046
dennmar <dennismariano3@gmail.com>
2017-08-29 16:30:27 -0700

    Update to include post, put, and delete routes for levels

--- routes/find.js
+++ routes/find.js
@@ -40,4 +40,65 @@ router.get( "/levels", function( req, res ) {
 	});
 });
 
+router.post( "/levels", function( req, res ) {
+	if ( req.user && req.user.username === "Admin" ) {
+		var newLevel = {
+			level: req.body.level,
+			difficulty: req.body.difficulty,
+			startingRotates: req.body.startingRotates
+		};
+		Level.create( newLevel, function( err, level ) {
+			if ( err ) {
+				console.log( err );
+			}
+			else {
+				res.redirect( "/find/levels" );
+			}
+		});
+	}
+	else {
+		req.flash( "userMessage", "Permission denied" );
+		res.redirect( "/" );
+	}
+});
+
+router.put( "/levels/:level_id", function( req, res ) {
+	if ( req.user && req.user.username === "Admin" ) {
+		var updatedLevel = {
+			level: req.body.level,
+			difficulty: req.body.difficulty,
+			startingRotates: req.body.startingRotates
+		}
+		Level.findByIdAndUpdate( req.params.level_id, updatedLevel, function( err, updatedLevel ) {
+			if ( err ) {
+				console.log( err );
+			}
+			else {
+				res.redirect( "/find/levels" );
+			}
+		});
+	}
+	else {
+		req.flash( "userMessage", "Permission denied" );
+		res.redirect( "/" );
+	}
+});
+
+router.delete( "/levels/:level_id", function( req, res ) {
+	if ( req.user && req.user.username === "Admin" ) {
+		Level.findByIdAndRemove( req.params.level_id, function( err ) {
+			if ( err ) {
+				console.log( err );
+			}
+			else {
+				res.redirect( "/find/levels" );
+			}
+		});
+	}
+	else {
+		req.flash( "userMessage", "Permission denied" );
+		res.redirect( "/" );
+	}
+});
+
 module.exports = router;
