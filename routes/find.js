d2f539a3a69e73e9b9089692c959139ad0b1821a
dennmar <dennismariano3@gmail.com>
2017-08-31 17:46:48 -0700

    Fix merge conflict in find.js

--- routes/find.js
+++ routes/find.js
@@ -41,64 +41,46 @@ router.get( "/levels", function( req, res ) {
 });
 
 router.post( "/levels", function( req, res ) {
-	if ( req.user && req.user.username === "Admin" ) {
-		var newLevel = {
-			level: req.body.level,
-			difficulty: req.body.difficulty,
-			startingRotates: req.body.startingRotates
-		};
-		Level.create( newLevel, function( err, level ) {
-			if ( err ) {
-				console.log( err );
-			}
-			else {
-				res.redirect( "/find/levels" );
-			}
-		});
-	}
-	else {
-		req.flash( "userMessage", "Permission denied" );
-		res.redirect( "/" );
-	}
+	var newLevel = {
+		level: req.body.level,
+		difficulty: req.body.difficulty,
+		startingRotates: req.body.startingRotates
+	};
+	Level.create( newLevel, function( err, level ) {
+		if ( err ) {
+			console.log( err );
+		}
+		else {
+			res.redirect( "/find/levels" );
+		}
+	});
 });
 
 router.put( "/levels/:level_id", function( req, res ) {
-	if ( req.user && req.user.username === "Admin" ) {
-		var updatedLevel = {
-			level: req.body.level,
-			difficulty: req.body.difficulty,
-			startingRotates: req.body.startingRotates
+	var updatedLevel = {
+		level: req.body.level,
+		difficulty: req.body.difficulty,
+		startingRotates: req.body.startingRotates
+	}
+	Level.findByIdAndUpdate( req.params.level_id, updatedLevel, function( err, updatedLevel ) {
+		if ( err ) {
+			console.log( err );
 		}
-		Level.findByIdAndUpdate( req.params.level_id, updatedLevel, function( err, updatedLevel ) {
-			if ( err ) {
-				console.log( err );
-			}
-			else {
-				res.redirect( "/find/levels" );
-			}
-		});
-	}
-	else {
-		req.flash( "userMessage", "Permission denied" );
-		res.redirect( "/" );
-	}
+		else {
+			res.redirect( "/find/levels" );
+		}
+	});
 });
 
 router.delete( "/levels/:level_id", function( req, res ) {
-	if ( req.user && req.user.username === "Admin" ) {
-		Level.findByIdAndRemove( req.params.level_id, function( err ) {
-			if ( err ) {
-				console.log( err );
-			}
-			else {
-				res.redirect( "/find/levels" );
-			}
-		});
-	}
-	else {
-		req.flash( "userMessage", "Permission denied" );
-		res.redirect( "/" );
-	}
+	Level.findByIdAndRemove( req.params.level_id, function( err ) {
+		if ( err ) {
+			console.log( err );
+		}
+		else {
+			res.redirect( "/find/levels" );
+		}
+	});
 });
 
 module.exports = router;
